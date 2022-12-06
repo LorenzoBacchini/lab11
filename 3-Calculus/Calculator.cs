3055801bb1ef80fd5c631a0dd7f1efd39229119f
Bacchini Lorenzo <lorenzo.bacchini4@studio.unibo.it>
2022-12-06 23:20:28 +0100

    Es 3 done

--- 3-Calculus/Calculator.cs
+++ 3-Calculus/Calculator.cs
@@ -1,3 +1,4 @@
+using System;
 using ComplexAlgebra;
 
 namespace Calculus
@@ -27,6 +28,97 @@ namespace Calculus
         public const char OperationPlus = '+';
         public const char OperationMinus = '-';
 
-        // TODO fill this class
+        /// <summary>
+        /// Mantiene il risultato temporaneo/parziale della calcolatrice
+        /// </summary>
+        private Complex _result = null;
+        
+        /// <summary>
+        /// Mantiene il valore attuale passato alla calcolatrice e quando chiamo (ComputeResult())
+        /// si salva al suo interno il risultato finale, questo perchè e proprio value che stampo
+        /// ad ogni operazione della calcolatrice per vedere a "shcermo" cosa sto facendo
+        /// </summary>
+        
+        ///Mantiene il segno dell'operazione da eseguire (+ o -)
+        public Complex Value { get; set; }
+
+        private char? _operation;
+
+        /// <summary>
+        /// Metodo MOLTO IMPORTANTE viene chiamato quando inserisco un (+ o -) nella
+        /// calcolatrice, si occupa di:
+        ///     - portare a null il valore di _operation
+        ///     - inizializzare il valore di _result a Value la prima volta che viene
+        ///       premuto un (+ o -)
+        ///     - compiere operazioni su _result ogni qualvolta si "schiacci" un tasto
+        ///       della calcolatrice
+        /// </summary>
+        public char? Operation
+        {
+            get => _operation;
+            set
+            {
+                if (value == null)
+                {
+                    _operation = value;
+                    return;
+                }
+                
+                _operation = value;
+                if (_result == null)
+                {
+                    _result = Value;
+                }
+                else
+                {
+                    ComputeIntermediateResult();
+                }
+                Value = null;
+            }
+        }
+
+        /// <summary>
+        /// Metodo usato internamente che calcola l'operazione richiesta su _result
+        /// al passaggio del parametro Value da (sommare/sottrarre) a _result
+        /// </summary>
+        private void ComputeIntermediateResult()
+        {
+            _result = _operation == OperationPlus ? _result.Sum(Value) : _result.Min(Value);
+        }
+
+        /// <summary>
+        /// Corrisponde al tasto = della calcolatrice, ci porta nella
+        /// condizione di avere il risultato nella Properties che verra poi
+        /// stampata, porta inoltre a null il valore di Operation
+        /// </summary>
+        public void ComputeResult()
+        {
+            Value = _result;
+            Operation = null;
+        }
+
+        /// <summary>
+        /// Metodo che crea la stringa da stampare ad ogni operazione,
+        /// stampa prima il valore dell'operazione poi il suo segno (+ o -)
+        /// se una delle due e null allora al suo posto metterà null
+        /// </summary>
+        /// <returns>la stringa completa</returns>
+        public override string ToString()
+        {
+            string cNumber = Value == null ? "null" : $"{Value}";
+            string sign = Operation == null ? "null" : $"{Operation}";
+            return $"{cNumber} , {sign}";
+        }
+
+        /// <summary>
+        /// Metodo che resetta la calcolatrice portando a null
+        /// il suo Value, Operation e _result
+        /// </summary>
+        public void Reset()
+        {
+            _result = null;
+            Value = null;
+            Operation = null;
+        }
     }
 }
